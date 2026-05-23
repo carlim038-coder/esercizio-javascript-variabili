44d4e28a83da7cd6ab36a0cb3470e4163c386641
iris <carlim038@gmail.com>
2026-05-23 19:34:08 +0200

    eseuzione esercizio finita

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 // Contenuto di main.js
-let num1 = 10;
+// Dichiarazione della variabile richiesta
+let num1 = 42;
 
+// Stampe su console
+console.log("Il valore della variabile num1 è: " + num1);
+console.log("Esercizio 1 completato con successo");
 
-console.log("Il valore di num1 è: " + num1);
+let num2 = 50;
+
+// Stampe su console
+console.log("Il valore della variabile num2 è: " + num2);
+console.log("Esercizio 2 completato con successo");
+
+// 2. Stampa a schermo il valore della variabile stringa
+let stringa = "Ciao a tutti!";
+console.log("Il valore della variabile stringa: " + stringa);
+
+// 3. Sostituisci il valore e stampalo di nuovo
+stringa = "Ora ho cambiato valore alla stringa";
+console.log("Il nuovo valore della variabile stringa: " + stringa);
+
+// 4. Dichiara una costante PIGRECO
+const PIGRECO = 3.14159265359;
+
+// 5. Visualizza a schermo il valore di PIGRECO
+console.log("Il valore di PIGRECO è: " + PIGRECO);
+
+// 6. Prova a cambiare il valore della costante
+// Se provi a decommentare la riga sotto, vedrai un errore nella console (F12)
+// PIGRECO = 3.14; 
+// Questa riga darà un errore perché PIGRECO è una costante 
+
+let PIGRECO = 4; 
+console.log("Se provassi a cambiare PIGRECO, il browser darebbe un errore: TypeError");
